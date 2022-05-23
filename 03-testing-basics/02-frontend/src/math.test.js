a05e930c9a625047ba087b0057cec88d34b72906
alexbjr369 <alexbjr369@gmail.com>
2022-05-23 20:17:38 -0300

    commit

--- 03-testing-basics/02-frontend/src/math.test.js
+++ 03-testing-basics/02-frontend/src/math.test.js
@@ -7,7 +7,7 @@ it('should summatize all number values in array', () => {
   const numbers = [1, 2, 3];
 
   // act
-  const result = add([1, 2, 3]);
+  const result = add(numbers);
 
   // assert
   const expectedResult = numbers.reduce(
@@ -16,4 +16,36 @@ it('should summatize all number values in array', () => {
   );
 
   expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if at least one invalid number is provided', () => {
+  const inputs = [1, 2, 'invalid'];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  const numbers = ['1', '2', '3'];
+
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (prevValue, currValue) => +prevValue + +currValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield 0 if an empty array is provided', () => {
+  const numbers = [];
+
+  const result = add(numbers);
+
+  expect(result).toBe(0);
+});
+
+it('should throw an error if no value is passed into the function', () => {
+  const result = add();
 });
