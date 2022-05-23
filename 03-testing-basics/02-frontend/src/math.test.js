7d53981163a1e85558ecf6c1af0b3c3bd818a105
alexbjr369 <alexbjr369@gmail.com>
2022-05-23 07:34:33 -0300

    commit

--- 03-testing-basics/02-frontend/src/math.test.js
+++ 03-testing-basics/02-frontend/src/math.test.js
@@ -1,6 +1,6 @@
 // import { test } from 'vitest';
 import { it, expect } from 'vitest';
-import { add } from 'math';
+import { add } from './math';
 
 it('should summatize all number values in array', () => {
   const result = add([1, 2, 3]);
