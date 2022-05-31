f88c7a636cffaf503256158670763c6e60a96cc6
alexbjr369 <alexbjr369@gmail.com>
2022-05-31 04:56:24 -0300

    comit

--- 06-advanced-testing-concepts/script.test.js
+++ 06-advanced-testing-concepts/script.test.js
@@ -1,10 +1,16 @@
 import { it } from 'vitest';
 import { generateToken } from './script';
 
-it('should generate a token value', () => {
+it('should generate a token value', () =>
+  new Promise((done) => {
     const testUserEmail = 'test@test.com';
 
     generateToken(testUserEmail, (err, token) => {
+      try {
         expect(token).toBeDefined();
+        done();
+      } catch (err) {
+        done(err);
+      }
     });
-});
+  }));
